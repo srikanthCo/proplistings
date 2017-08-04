4344e2d81ba3a7f62e01aad21f98113d81886ffb
srkanth30 <kudikyalasrikanth@gmail.com>
2017-08-04 13:12:15 +0530

    ignoring environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebase:{
-      apiKey: "",
-      authDomain: "",
-      databaseURL: "",
-      projectId: "",
-      storageBucket: "",
-      messagingSenderId: ""
+      apiKey: "AIzaSyBcDeyDFLCHI7K34_ZeOzKXx-wFJK0keg0",
+      authDomain: "sampler-5e4db.firebaseapp.com",
+      databaseURL: "https://sampler-5e4db.firebaseio.com",
+      projectId: "sampler-5e4db",
+      storageBucket: "sampler-5e4db.appspot.com",
+      messagingSenderId: "111056068886"
     }
     
   };
