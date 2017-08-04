ef4c4bdf42f94bead3da086014ec3ef1314f31cd
srkanth30 <kudikyalasrikanth@gmail.com>
2017-08-04 13:16:00 +0530

    revert changes for environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebase:{
-      apiKey: "AIzaSyBcDeyDFLCHI7K34_ZeOzKXx-wFJK0keg0",
-      authDomain: "sampler-5e4db.firebaseapp.com",
-      databaseURL: "https://sampler-5e4db.firebaseio.com",
-      projectId: "sampler-5e4db",
-      storageBucket: "sampler-5e4db.appspot.com",
-      messagingSenderId: "111056068886"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: ""
     }
     
   };
